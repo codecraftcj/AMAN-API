8c58012850f0d7d54ce2dacb9eb26a8d185a3745
cj <codecraftcj@gmail.com>
2025-01-26 20:05:08 +0800

    adding todo

--- main.py
+++ main.py
@@ -222,5 +222,6 @@ def protected():
     
     return jsonify(logged_in_as=user.name), 200
 
+# @TODO: connect to GUI 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
